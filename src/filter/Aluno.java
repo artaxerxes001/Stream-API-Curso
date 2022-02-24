7a01e9e89b4af7537a309ed67971c4ef6d49ec68
artaxerxes001 <artaxerxesnazare@gmail.com>
2022-02-24 21:35:57 +0100

    finalizado reduce

--- src/filter/Aluno.java
+++ src/filter/Aluno.java
@@ -1,11 +1,17 @@
 package filter;
 
 public class Aluno {
-    final String nome;
-    final double nota;
+    final public String nome;
+    final public double nota;
+    final public boolean bomComportamento;
 
     public Aluno(String nome, double nota) {
+        this(nome, nota, true);
+    }
+
+    public Aluno(String nome, double nota, boolean bomComportamento) {
         this.nome = nome;
         this.nota = nota;
+        this.bomComportamento = bomComportamento;
     }
 }
